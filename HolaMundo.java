26bcdd995c3dd902ace0e3ec17967fd01328b3a3
ladobled <digopu@hotmail.com>
2014-09-11 11:59:25 +0200

    Update HolaMundo.java

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,9 +1,8 @@
 public class HolaMundo{
-	public static void main(String args[]){
 	
-int n = 0;
-String h = "HijoPuta";
-String saludo = "Hola";
-
+	public static void main(String args[]){
+		int n = 0;
+		String h = "HijoPuta";
+		String saludo = "Hola";
 	}
 }
